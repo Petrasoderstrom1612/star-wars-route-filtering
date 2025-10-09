d6b524d2afc0063eceecf43584768d8631f4f4e0
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-10-09 08:56:38 +0200

    added the same kind of function but this time for buttons and with stateSetter function

--- src/Characters.jsx
+++ src/Characters.jsx
@@ -35,23 +35,40 @@ const possiblyFilteredCharacters = filterParam ? swCharacters.filter(oneChar => 
       return `?${searchParametersSegment.toString()}` //you must return something and convert it in the "?string" from
     }
 
+      //This function will only modify the param with key "type" as per buttons
+
+    const setNewSearchParamsSegment = (key, value) => {
+      setSearchParams(prevParams => {
+        if (value === null){
+          prevParams.delete(key)
+        } else {
+          prevParams.set(key,value)
+        }
+        return prevParams
+      })
+    } 
+
   return (
   <main>
     <h1>Star wars characters</h1>
     <div>
       <Link to={generateNewSearchParamsSegment("type", "jedi")}>Jedis</Link>
       <Link to={generateNewSearchParamsSegment("type", "sith")}>Siths</Link>
-      <Link to={generateNewSearchParamsSegment("type", null)}>Clear</Link>
+      <Link to={generateNewSearchParamsSegment("type", null)}>Clear filters</Link>
     </div>
     <nav>
-      {/* <button onClick={() => setSearchParams({type: "jedi"}) }>Jedis </button>
-      <button onClick={() => setSearchParams("?type=sith")}>Siths </button>
-      <button onClick={() => setSearchParams("type=animal")}>Animals</button>
-      <Link to=".">Clear search</Link> It would also work with empty ""  meaning clear what is there */}
+      <button onClick={() => setNewSearchParamsSegment("type", "jedi") }>Jedis </button>
+      <button onClick={() => setNewSearchParamsSegment("type", "sith")}>Siths </button>
+      <button onClick={() => setNewSearchParamsSegment("type", null)}>Clear filters</button>
     </nav>
     {characterEl}
   </main>
   );
 };
+
+{/* <button onClick={() => setSearchParams({type: "jedi"}) }>Jedis </button>
+<button onClick={() => setSearchParams("?type=sith")}>Siths </button>
+<button onClick={() => setSearchParams("type=animal")}>Animals</button>
+<Link to=".">Clear search</Link> //It would also work with empty ""  meaning clear what is there */}
 
 export default Characters;
